ac337f787f8e16362e112857c2a7f7a62a312eda
psidae <akson.rill@gmail.com>
2020-06-18 12:17:40 +0300

    Example

--- app/main.rb
+++ app/main.rb
@@ -11,4 +11,9 @@ get '/about' do
   slim :about
 end
 
+# Qwertyuiopasdfghjklzxcvbnm1234567890987654321234567890-0987654321234567890-098765432Qwertyuiopasdfghjklzxcvbnm1234567890987654321234567890-0987654321234567890-098765432
+get '/faq' do
+  slim :faq
+end
+
 set :port, 3000
